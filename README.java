9405662d6109709698d3b3ac4083fb35d8c052df
abi123naya <connect2abinaya@gmail.com>
2016-07-08 11:17:02 +0530

    Update and rename README.md to README.java
    finidh

---
+++ README.java
@@ -0,0 +1,30 @@
+# half
+import.java.util.*;
+class Larger
+{int big; 
+public static void main(String[] args)
+  { int a=5,b=10,c=1;
+        if(a>b&&a>c)
+    { big= a;
+    System.out.println(big);}
+    else
+    {if(b>a&&b>c)
+       big=b;{ 
+      System.out.println(big);}
+     }
+  else
+  {big =c;
+  System.out.println(big);}
+  
+  
+  
+  
+  }
+
+
+
+
+
+
+
+}
